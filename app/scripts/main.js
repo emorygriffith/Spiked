bf0a5a946d75462f45c9efc93f758ccd73202ee9
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-16 14:53:38 -0400

    almost done JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 var newsTemplate = $('#newsApi').html();
 var showNews = _.template(newsTemplate);
 var newsUrl='http://restaurantapi.apiary-mock.com/news/latest';
@@ -10,24 +9,59 @@ $.getJSON(newsUrl).done(
 
 
 
+
+
 var specialTemplate = $('#specialApi').html();
 var showSpecial = _.template(specialTemplate);
 var specialUrl='http://restaurantapi.apiary-mock.com/menu/special';
 
+var special_ID;
+$.getJSON(specialUrl).done(function(special_data) {
+      special_ID = special_data.menu_item_id;
+      console.log(special_ID);
+      });
+
+
 $.getJSON(specialUrl).done(
-  function (pullingApi) {
-    $('.special').append( showSpecial(pullingApi) );
-});
+        function (pullingApi) {
+      $('.special').append( showSpecial(pullingApi) );
+      });
+
 
 var menuTemplate = $('#menuApi').html();
 var showMenu = _.template(menuTemplate);
 var menuUrl='http://restaurantapi.apiary-mock.com/menu';
 
+
+
+var app_ID;
+var sid_ID;
+var ent_ID;
+
 $.getJSON(menuUrl).done(
   function (pullingApi) {
+    pullingApi.appetizers.forEach(
+      function(pullingFromArray) {
+        app_ID = pullingFromArray.id;
+        console.log(app_ID);
+         $('.appetizers').append( showMenu(pullingFromArray));
+          });
+
+
     pullingApi.entrees.forEach(
       function(pullingFromArray) {
-         $('.menu').append( showMenu(pullingFromArray));
-  });
+        sid_ID = pullingFromArray.id;
+        console.log(sid_ID);
+         $('.entrees').append( showMenu(pullingFromArray));
+          });
 
-});
+    pullingApi.sides.forEach(
+      function(pullingFromArray) {
+        ent_ID = pullingFromArray.id;
+        console.log(ent_ID);
+         $('.sides').append( showMenu(pullingFromArray));
+       });
+
+
+
+    });
