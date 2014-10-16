ba402c72f1d361bbd08fd692cbf867765b1fd0ff
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-16 17:30:01 -0400

    specials needs to be done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,7 @@
+
+/*.........................................................*/
+/*.........................................................*/
+
 var newsTemplate = $('#newsApi').html();
 var showNews = _.template(newsTemplate);
 var newsUrl='http://restaurantapi.apiary-mock.com/news/latest';
@@ -8,22 +12,25 @@ $.getJSON(newsUrl).done(
 });
 
 
+/*.........................................................*/
+/*.........................................................*/
 
 
 
 var specialTemplate = $('#specialApi').html();
 var showSpecial = _.template(specialTemplate);
 var specialUrl='http://restaurantapi.apiary-mock.com/menu/special';
-
 var special_ID;
-$.getJSON(specialUrl).done(function(special_data) {
+
+  $.getJSON(specialUrl).done(
+    function(special_data) {
       special_ID = special_data.menu_item_id;
-      console.log(special_ID);
-      });
+        console.log(special_ID);
+        });
 
 
-$.getJSON(specialUrl).done(
-        function (pullingApi) {
+  $.getJSON(specialUrl).done(
+    function (pullingApi) {
       $('.special').append( showSpecial(pullingApi) );
       });
 
@@ -31,27 +38,24 @@ $.getJSON(specialUrl).done(
 var menuTemplate = $('#menuApi').html();
 var showMenu = _.template(menuTemplate);
 var menuUrl='http://restaurantapi.apiary-mock.com/menu';
-
-
-
 var app_ID;
 var sid_ID;
 var ent_ID;
 
-$.getJSON(menuUrl).done(
-  function (pullingApi) {
-    pullingApi.appetizers.forEach(
-      function(pullingFromArray) {
-        app_ID = pullingFromArray.id;
-        console.log(app_ID);
+  $.getJSON(menuUrl).done(
+    function (pullingApi) {
+
+      pullingApi.appetizers.forEach(
+        function(pullingFromArray) {
+          app_ID = pullingFromArray.id;
+          console.log(app_ID);
          $('.appetizers').append( showMenu(pullingFromArray));
           });
 
-
-    pullingApi.entrees.forEach(
-      function(pullingFromArray) {
-        ent_ID = pullingFromArray.id;
-        console.log(ent_ID);
+      pullingApi.entrees.forEach(
+        function(pullingFromArray) {
+          ent_ID = pullingFromArray.id;
+          console.log(ent_ID);
          $('.entrees').append( showMenu(pullingFromArray));
           });
 
@@ -60,13 +64,12 @@ $.getJSON(menuUrl).done(
         sid_ID = pullingFromArray.id;
         console.log(sid_ID);
          $('.sides').append( showMenu(pullingFromArray));
-       });
+          });
+
        if (special_ID===ent_ID) {
           (special_data=pullingFromArray.item)
           (special_data=pullingFromArray.price)
           (special_data=pullingFromArray.description)
-          //console.log(pullingFromArray.description);
-       }
-
+          }
 
     });
